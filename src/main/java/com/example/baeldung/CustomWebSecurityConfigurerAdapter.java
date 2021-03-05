d8eb5af91890b40c42379eb1c18526a5fe2aaeea
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-04 21:54:59 -0800

    Added a new admin role. Fixed security adapter by adding missing annotations.

--- src/main/java/com/example/baeldung/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/example/baeldung/CustomWebSecurityConfigurerAdapter.java
@@ -2,12 +2,16 @@ package com.example.baeldung;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
     
     @Autowired
@@ -16,8 +20,9 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("user1").password(passwordEncoder().encode("user1pass"))
-            .authorities("ROLE_USER");
+            .withUser("user1").password(passwordEncoder().encode("user1pass")).roles("USER")
+            .and()
+            .withUser("admin").password(passwordEncoder().encode("yankee")).roles("ADMIN");
     }
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +31,10 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
             .anyRequest().authenticated()
             .and()
             .httpBasic()
-            .authenticationEntryPoint(authenticationEntryPoint);
+            .authenticationEntryPoint(authenticationEntryPoint)
+            .and()
+            .logout()
+            .logoutSuccessUrl("/login.html");
     }
 
     @Bean
